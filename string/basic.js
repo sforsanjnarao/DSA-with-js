7c39a7b5fa69d0495a015bd4ca949ecf7e9db982
sforsanjnarao <sanjnarao26@gmail.com>
2025-04-28 19:21:34 +0530

    Add frequencyOfEachElement function to count occurrences of elements in an array

--- string/basic.js
+++ string/basic.js
@@ -85,3 +85,17 @@ function checkTwoStringAnagramOrNot(str1,str2){
     if (str1!==str2) return console.log('Not anagram')
     
 }
+
+
+//68
+function frequencyOfEachElement(arr){
+    let map=new Map()
+    for(let i=0; i<arr.length; i++){
+        if(map.has(arr[i])){
+            map.set(arr[i],map.get(arr[i])+1)
+        }
+        else{
+            map.set(arr[i],1)
+        }
+    }
+}
