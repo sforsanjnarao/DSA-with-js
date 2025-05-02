0ebee40b035c52929ca9030edb4795b68163efa1
sforsanjnarao <sanjnarao26@gmail.com>
2025-05-02 05:31:11 +0530

    Add factorial function and comment out test cases in basic.js

--- recursion/basic.js
+++ recursion/basic.js
@@ -28,4 +28,13 @@ function sum(n){
     if(n==1) return 1
     return n+sum(n-1)
 }
-console.log(sum(5))
+// console.log(sum(5))
+
+
+
+//factorial
+function factorial(n){
+    if(n==1) return n;
+    return n*factorial(n-1)
+}
+// console.log(factorial(6))
