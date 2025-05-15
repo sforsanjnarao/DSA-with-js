80f32aaf7bbe217f9eac891d06472e7231453a02
sforsanjnarao <sanjnarao26@gmail.com>
2025-05-15 10:46:33 +0530

    Add maximumToys function to calculate the maximum number of toys purchasable within a budget

---
+++ hackerRanckQestions/arrayAndList/markAndToys.js
@@ -0,0 +1,17 @@
+// Mark and Toys – Maximize the Fun
+
+// Description:
+// Mark has a fixed amount of money and a list of toy prices. 
+// Your task is to help him buy the maximum number of toys without exceeding his budget. 
+// Return the maximum count of toys he can purchase.
+function maximumToys(prices, k) {
+    price.sort((a,b)=>a-b)
+    let count=0; 
+    let price=k
+    for(let i=0; i<prices.length; i++){
+        while(k>=0){
+            price=k-
+        }
+        return count++
+    }
+}
